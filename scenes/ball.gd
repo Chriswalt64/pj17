4fca85c03c0035a30c96c16c4fe7cff816a79d46
Chris <walters.chris64@gmail.com>
2025-07-29 22:27:51 -0400

    catching things up

--- scenes/ball.gd
+++ scenes/ball.gd
@@ -1,12 +1,14 @@
 extends CharacterBody2D
 
 signal releaseBall
+signal lose
+var flag : bool
 const BOUNDS = Vector2(0, 1280)
 var release : bool
 @onready var animation = $"../AnimationPlayer"
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.3
+	flag = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -15,6 +17,9 @@ func _process(delta):
 	if release:
 		animation.pause()
 		velocity += get_gravity() * delta
+	
+	if is_on_floor() && $"../Win_Lose".text != "You Win!":
+		emit_signal("lose")
 
 	move_and_slide()
 func _on_release_ball():
@@ -33,3 +38,15 @@ func _on_node_2d_cup_entered():
 func _on_exit_timeout():
 	Autoload.time += 1
 	get_tree().change_scene_to_file("res://scenes/proto_room.tscn")
+
+
+func _on_lose():
+	if !flag:
+		flag = true
+		$"../Win_Lose".text = "You Lose!"
+		$"../Exit".start()
+
+
+func _on_area_2d_body_entered(body):
+	if is_on_floor() && $"../Win_Lose".text != "You Win!":
+		emit_signal("lose")
